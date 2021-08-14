f937c9cf6dd6026b49772acc0d2e067734f76cba
Henry <henry983615@gmail.com>
2021-08-15 03:24:25 +1200

    added a update checker

--- installer.sh
+++ installer.sh
@@ -4,9 +4,9 @@ echo "[+] Updating Package Manager..."
 sudo apt-get update
 
 echo "[+] Installing NodeJS-16..."
-sudo apt-get install curl 
+sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
-sudo apt-get install nodejs npm
+sudo apt-get install nodejs npm git -y
 
 echo "[+] Installing Modules..."
 npm install discord.js process semver e621 mongodb mongoose fs --save
--- watchdog.sh
+++ watchdog.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 while true; do
+
+    #Apply any updates
+    echo "[+] Checking and Downloading Git Updates..."
+    git stash
+    git checkout master
+    git branch new-branch-to-save-current-commits
+    git fetch --all
+    git reset --hard origin/master
+    git stash pop
+    echo "[+] Done."
+
+    echo "[+] Starting Discord Bot..."
     node ./app.js
     TIME=$( date '+%F_%H:%M:%S' )
     echo $'\n----------------------------------'
