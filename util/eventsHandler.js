274caced255969ece354650b07cd35f226df22ae
Henry Oliver <henry983615@gmail.com>
2022-01-02 19:20:10 +1300

    Implement a filereader for the event handler

--- util/eventsHandler.js
+++ util/eventsHandler.js
@@ -2,17 +2,40 @@ const fs = require('fs');
 
 const debugging = require("./debugging.js");
 
-const eventObject ={
-    Date: Date.now(),
-    String: "Error"
+function eventObject(date, data) {
+    this.Date = date;
+    this.Data = data;
 }
 
 var eventObjects = [];
 
 function initEvents(){
     try {
+        //Read from file and split each event up
         const data = fs.readFileSync('./util/events', 'utf8');
-        console.log(data);
+        var events = data.split('\n');
+
+        //Convert lines to events and dates and add them to our events list
+        events.forEach(line => {
+            try{
+                var linedata = line.split(',');
+
+                let datestamp = Date.parse(linedata[0]);
+                let data = (linedata[1]);
+                let thing = new eventObject(datestamp, data);
+
+                if (isNaN(datestamp) === false){
+                    eventObjects.push(new eventObject(datestamp, data));
+                }
+            }
+            catch (err){console.log(err);}
+
+        });
+
+        console.log("--------");
+        console.log(eventObjects);
+        console.log("--------");
+
     } catch (err) {
         debugging.chickenScratch(err, debugging.DEBUGLVLS.WARN);
         console.error(err)
